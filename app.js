b9386f3304ef5f09de7e4063922a12384fd3dc21
Đoàn Thị Như Ý <116414562+doannhuyb2207580@users.noreply.github.com>
2025-07-11 22:14:56 +0700

    Cài đặt xử lý lỗi

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 
 const app = express();
 app.use(cors());
@@ -9,6 +10,18 @@ app.use("/api/contacts", contactsRouter);
 
 app.get("/", (req, res) => {
     res.json({message: "Welcome to contact book application."});
+});
+
+// Handle 404 response
+app.use((req, res, next0) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+
+// Define error-handling middleware last, after other app.use() and routes calls
+app.use((err, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal Server Error",
+    });
 });
 
 module.exports = app;
